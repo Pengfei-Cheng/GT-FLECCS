8100a8367691c7f35f9be04f5ab5ded832924e7f
Pengfei Cheng <johnickcheng@gmail.com>
2023-02-19 21:10:05 -0500

    fix electricity price signal bug

--- src/pyomo_model/model.py
+++ src/pyomo_model/model.py
@@ -166,7 +166,7 @@ def const_model(n_day=2, elec_price_signal="MiNg_150_NYISO", week_diff=1):
                     m.x_cost_NGCC_VOM[i, s] - m.x_cost_PCC_VOM[i, s] - m.x_cost_DAC_VOM[i, s] - m.x_cost_PCC_compr_VOM[i, s] - m.x_cost_DAC_compr_VOM[i, s]
                     for i in set_hour
                 ) / len(set_hour) * 364 * 24
-                - ((a_cost_sorbent * m.x_sorbent_total * 3000 + a_cost_adsorb * m.x_air_adsorb_max * 48000) * 0.05 + 2 * 110000)
+                - scenario_prob[s] * ((a_cost_sorbent * m.x_sorbent_total * 3000 + a_cost_adsorb * m.x_air_adsorb_max * 48000) * 0.05 + 2 * 110000)
             ) * (1 - tax_r) * sum(1 / (1 + int_r) ** j for j in range(2, 21 + 1)) \
             - scenario_prob[s] * (a_cost_sorbent * m.x_sorbent_total * 3000 + a_cost_adsorb * m.x_air_adsorb_max * 48000) * (1 + 0.0311 + 0.0066 + 0.1779) * ( 0.3 + 0.7 / (1 + int_r) - sum(tax_r * depreciate_r * ((1 - depreciate_r) ** j) * ((1 + int_r) ** (- j - 2)) for j in range(19 + 1)) )
     objs = {s: _obj for s in set_scenario}
